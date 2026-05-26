7630d73c5fa37558ce73e59c171cd01bdf7aa8e7
Jin-A Park <jinashii1013@skku.edu>
2026-05-26 15:40:29 +0900

    controller fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,15 @@
-INSERT INTO article(title, content) VALUES('가가가가', '11111');
-INSERT INTO article(title, content) VALUES('나나나나', '22222');
-INSERT INTO article(title, content) VALUES('다다다다', '33333');
 INSERT INTO article(title, content) VALUES('당신의 인생 영화는?', '댓글 고');
 INSERT INTO article(title, content) VALUES('당신의 소울 푸드는?', '댓글 고고');
 INSERT INTO article(title, content) VALUES('당신의 취미는?', '댓글 고고고');
 
-INSERT INTO comment(article_id, nickname, body) VALUES(4, 'PARK', '굿 윌 헌팅');
-INSERT INTO comment(article_id, nickname, body) VALUES(4, 'KIM', '아이 엠 샘');
-INSERT INTO comment(article_id, nickname, body) VALUES(4, 'CHOI', '쇼생크 탈출');
+INSERT INTO comment(article_id, nickname, body) VALUES(1, 'PARK', '굿 윌 헌팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(1, 'KIM', '아이 엠 샘');
+INSERT INTO comment(article_id, nickname, body) VALUES(1, 'CHOI', '쇼생크 탈출');
 
-INSERT INTO comment(article_id, nickname, body) VALUES(5, 'PARK', '치킨');
-INSERT INTO comment(article_id, nickname, body) VALUES(5, 'KIM', '샤브샤브');
-INSERT INTO comment(article_id, nickname, body) VALUES(5, 'CHOI', '초밥');
+INSERT INTO comment(article_id, nickname, body) VALUES(2, 'PARK', '치킨');
+INSERT INTO comment(article_id, nickname, body) VALUES(2, 'KIM', '샤브샤브');
+INSERT INTO comment(article_id, nickname, body) VALUES(2, 'CHOI', '초밥');
 
-INSERT INTO comment(article_id, nickname, body) VALUES(6, 'PARK', '조깅');
-INSERT INTO comment(article_id, nickname, body) VALUES(6, 'KIM', '유튜브 시청');
-INSERT INTO comment(article_id, nickname, body) VALUES(6, 'CHOI', '독서');
+INSERT INTO comment(article_id, nickname, body) VALUES(3, 'PARK', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES(3, 'KIM', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES(3, 'CHOI', '독서');
