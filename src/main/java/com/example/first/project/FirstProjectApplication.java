77d43c4458d8eb619f17aa402b1da20d7a4353e9
[GithubName] <jinashii1013@skku.edu>
2026-06-28 21:22:49 +0900

    before restructuring

--- src/main/java/com/example/first/project/FirstProjectApplication.java
+++ src/main/java/com/example/first/project/FirstProjectApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstProjectApplication {
+public class
+FirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectApplication.class, args);
